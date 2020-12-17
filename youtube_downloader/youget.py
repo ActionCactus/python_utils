b9683c38aa564338e159ddecd33cb3da2e0f41e3
nick.atkocaitis <n.atkocaitis@gmail.com>
2020-12-17 04:08:44 -0500

    Learned venvs and improved error reporting.

--- youtube_downloader/youget.py
+++ youtube_downloader/youget.py
@@ -3,6 +3,7 @@
 import argparse
 from pytube import YouTube
 from pytube.streams import Stream
+from console import bg
 
 
 def get_configured_destination():
@@ -28,11 +29,11 @@ arg_parser = argparse.ArgumentParser(description="Download Youtube videos!")
 arg_parser.add_argument(
     f"{URLS_ARG}",
     nargs="+",
-    help="A space-delimited list of URLs containing videos to download."
+    help="A space-delimited list of URLs containing videos to download.",
 )
 arg_parser.add_argument(
     f"--{DESTINATION_ARG}",
-    help="The destination directory in which these videos should be saved."
+    help="The destination directory in which these videos should be saved.",
 )
 
 
@@ -40,14 +41,24 @@ inputs = arg_parser.parse_known_args()[0].__dict__
 urls = inputs[URLS_ARG]
 destination = inputs.get(DESTINATION_ARG, get_configured_destination())
 
-
+err = False
 for url in urls:
-    handle = YouTube(url, on_progress_callback=download_progress_callback)
+    try:
+        handle = YouTube(url, on_progress_callback=download_progress_callback)
+        handle.prefetch()
 
-    print(f"Downloading '{handle.title}' by '{handle.author}'")
+        print(f"Downloading '{handle.title}' by '{handle.author}'")
 
-    cleaned_title = clean_vid_title(handle.title)
-    handle.streams.get_highest_resolution().download(
-        output_path=destination,
-        filename=cleaned_title
-    )
+        cleaned_title = clean_vid_title(handle.title)
+        stream = handle.streams.get_highest_resolution()
+
+        if not stream:
+            raise Exception(f"No download stream could be located!")
+
+        stream.download(output_path=destination, filename=cleaned_title)
+    except Exception as e:
+        err = True
+        print(f"{bg.red}Issue while downloading {url}: {e}{bg.default}")
+
+exit_code = 1 if err else 0
+quit(exit_code)
